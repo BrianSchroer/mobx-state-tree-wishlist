84fceacd38ceae71839a2b3e427f31992827bac3
Brian Schroer <brianschroer@gmail.com>
2018-01-11 09:51:01 -0600

    Remove unnecessary CRA stuff and add Santa logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/santa-claus.png';
 
 class App extends Component {
   render() {
@@ -8,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WishList</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
