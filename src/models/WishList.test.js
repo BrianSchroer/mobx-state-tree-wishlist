7681485777e818ec0859a3e542311059185a33a4
Brian Schroer <brianschroer@gmail.com>
2018-01-12 08:00:56 -0600

    refactored tests

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -8,68 +8,51 @@ const testInput = {
   image: 'https://images-na.ssl-images-amazon.com/images/I/51LmtX5KPAL._SX406_BO1,204,203,200_.jpg'
 };
 
-describe('WishListItem', () => {
-  it('create() without input should return "empty" item', () => {
-    snapshotHelper.test(WishListItem.create());
-  });
-
-  it('.create() without input should return expected values', () => {
-    snapshotHelper.test(WishListItem.create(testInput));
-  });
-
-  it('.create() without image should default to empty string for image', () => {
-    const input = {
-      name: 'Chronicles of Narnia Box Set - C.S. Lewis',
-      price: 28.83
-    };
-
-    snapshotHelper.test(WishListItem.create(input));
-  });
-
-  it('.changeName() should change name', () => {
-    snapshotHelper.test(
-      WishListItem.create(testInput),
-      item => item.changeName('new name'));
-  });
-
-  it('.changePrice() should change price', () => {
-    snapshotHelper.test(
-      WishListItem.create(testInput),
-      item => item.changePrice(12.34));
-  });
-
-  it('.changeImage() should change image', () => {
-    snapshotHelper.test(
-      WishListItem.create(testInput),
-      item => item.changeImage('new image'));
-  });
-});
+const testInputs = [1, 2, 3].map(i => ({ name: `name${i}`, price: i, image: `image${i}` }));
 
 describe('WishList', () => {
-  it('.create() should returns expected items', () => {
-    const inputItems = [1, 2, 3].map(i => ({ name: `name${i}`, price: i, image: `image${i}` }));
-    snapshotHelper.test(WishList.create({ items: inputItems }));
+  it('.create() should return expected items', () => {
+    snapshotHelper.test(WishList.create({ items: testInputs }));
   });
 
-  it('.create() without input returns empty items array', () => {
-    const output = WishList.create();
-    expect(output.items.length).toBe(0);
+  it('.create() without input should return empty items array', () => {
+    snapshotHelper.test(
+      WishList.create(),
+      list => expect(list.items.length).toBe(0));
   });
 
   it('.add(object) should add item', () => {
     snapshotHelper.test(
       WishList.create(),
-      list =>
-        list.add({ name: 'newName', price: 23.45, image: 'new image' }));
+      list => list.add({ name: 'newName', price: 23.45, image: 'new image' }));
   });
 
   it('.add(WishListItem) should add item', () => {
     snapshotHelper.test(
       WishList.create(),
-      list =>
-        list.add(WishListItem.create({
-          name: 'newName', price: 23.45, image: 'new image'
-        })));
+      list => list.add(WishListItem.create({ name: 'newName', price: 23.45, image: 'new image' })));
+  });
+
+  it('.remove() should remove item', () => {
+    snapshotHelper.test(
+      WishList.create({ items: testInputs }),
+      list => {
+        const { items } = list;
+        const itemCount = items.length;
+        list.remove(items[1]);
+        expect(items.length).toEqual(itemCount - 1);
+      });
+  });
+
+  it('.items[n].remove() should remove item', () => {
+    snapshotHelper.test(
+      WishList.create({ items: testInputs }),
+      list => {
+        const { items } = list;
+        const itemCount = items.length;
+        items[1].remove();
+        expect(items.length).toEqual(itemCount - 1);
+      });
   });
 
   it('.totalPrice should return sum of item prices', () => {
@@ -80,7 +63,7 @@ describe('WishList', () => {
     expect(list.totalPrice).toBe(23.68);
   })
 
-  it('.totalPrice should only be recalculated when necessary', () => {
+  it('.totalPrice should only be recalculated when price is changed or item is added/removed', () => {
     const list = WishList.create({ items: [testInput] });
     expect(list.totalPrice).toEqual(testInput.price);
 
@@ -106,5 +89,9 @@ describe('WishList', () => {
     list.items[1].changePrice(12.34);
     expect(list.totalPrice).toEqual(24.68);
     expect(recalculateCount).toBe(3);
+
+    list.remove(list.items[0]);
+    expect(list.totalPrice).toEqual(12.34);
+    expect(recalculateCount).toBe(4);
   });
 });
---
+++ src/models/WishListItem.test.js
@@ -0,0 +1,45 @@
+import { WishListItem } from './WishList';
+import { mobxSnapshotHelper as snapshotHelper } from '../util/testHelpers';
+
+const testInput = {
+  name: 'Chronicles of Narnia Box Set - C.S. Lewis',
+  price: 28.83,
+  image: 'https://images-na.ssl-images-amazon.com/images/I/51LmtX5KPAL._SX406_BO1,204,203,200_.jpg'
+};
+
+describe('WishListItem', () => {
+  it('create() without input should return "empty" item', () => {
+    snapshotHelper.test(WishListItem.create());
+  });
+
+  it('.create() without input should return expected values', () => {
+    snapshotHelper.test(WishListItem.create(testInput));
+  });
+
+  it('.create() without image should default to empty string for image', () => {
+    const input = {
+      name: 'Chronicles of Narnia Box Set - C.S. Lewis',
+      price: 28.83
+    };
+
+    snapshotHelper.test(WishListItem.create(input));
+  });
+
+  it('.changeName() should change name', () => {
+    snapshotHelper.test(
+      WishListItem.create(testInput),
+      item => item.changeName('new name'));
+  });
+
+  it('.changePrice() should change price', () => {
+    snapshotHelper.test(
+      WishListItem.create(testInput),
+      item => item.changePrice(12.34));
+  });
+
+  it('.changeImage() should change image', () => {
+    snapshotHelper.test(
+      WishListItem.create(testInput),
+      item => item.changeImage('new image'));
+  });
+});
