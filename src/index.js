0274179b389a3708903764b5c03677ce1368a65c
Brian Schroer <brianschroer@gmail.com>
2018-01-12 10:31:25 -0600

    Added hot module reloading

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { onSnapshot } from 'mobx-state-tree';
+import { getSnapshot, onSnapshot } from 'mobx-state-tree';
 import './assets/index.css';
-import App from './components/App';
 import { WishList } from './models/WishList';
+import App from './components/App';
 
-const defaultState = {
-  items: [
-    {
-      name: 'Lego Mindstorms EV3',
-      price: 349.9,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SY355_.jpg'
-    },
-    {
-      name: 'Chronicles of Narnia Box Set - C.S. Lewis',
-      price: 28.83,
-      image: 'https://images-na.ssl-images-amazon.com/images/I/51LmtX5KPAL._SX406_BO1,204,203,200_.jpg'
-    }
-  ]
-};
+function initializeWishList() {
+  const defaultState = {
+    items: [
+      {
+        name: 'Lego Mindstorms EV3',
+        price: 349.9,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SY355_.jpg'
+      },
+      {
+        name: 'Chronicles of Narnia Box Set - C.S. Lewis',
+        price: 28.83,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51LmtX5KPAL._SX406_BO1,204,203,200_.jpg'
+      }
+    ]
+  };
 
-let state = defaultState;
+  let state = defaultState;
 
-const json = localStorage.getItem('wishlistapp');
+  const json = localStorage.getItem('wishlistapp');
 
-if (json) {
-  const persistedState = JSON.parse(json);
-  //if (WishList.is(json)) { // (persisted state matches expected shape)
-  state = persistedState;
-  //}
+  if (json) {
+    const persistedState = JSON.parse(json);
+    // if (WishList.is(json)) { // (persisted state matches expected shape)
+    state = persistedState;
+    //}
+  }
+
+  return WishList.create(state);
 }
 
-const wishList = WishList.create(state);
+function setUpLocalStoragePersistence() {
+  onSnapshot(wishList, snapshot =>
+    localStorage.setItem('wishlistapp', JSON.stringify(snapshot)));
+}
 
-onSnapshot(wishList, snapshot =>
-  localStorage.setItem('wishlistapp', JSON.stringify(snapshot)));
+function setUpHotModuleReloading() {
+  if (!module.hot) {
+    return;
+  }
 
-ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+  // Reload after change to App component or one its child components:
+  module.hot.accept(['./components/App'], () => {
+    renderApp();
+  });
+
+  // Reload after change to model
+  module.hot.accept(['./models/WishList.js'], () => {
+    const snapshot = getSnapshot(wishList);
+    wishList = WishList.create(snapshot);
+    renderApp();
+  });
+}
+
+function renderApp() {
+  ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+}
+
+let wishList = initializeWishList();
+
+setUpLocalStoragePersistence();
+
+renderApp();
+
+setUpHotModuleReloading();
